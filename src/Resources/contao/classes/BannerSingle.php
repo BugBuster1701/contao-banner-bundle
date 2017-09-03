ca0a38a2850e8de072a5e8f638c6617782c87e2c
Glen Langer <contao.modul@glen-langer.de>
2017-09-03 21:54:15 +0200

    Aufruf setStaticUrls wegen TL_FILES_URL

--- src/Resources/contao/classes/BannerSingle.php
+++ src/Resources/contao/classes/BannerSingle.php
@@ -61,6 +61,9 @@ class BannerSingle extends \Frontend
         $this->strTemplate        = $strTemplate;
         $this->Template           = $objTemplate;
         $this->arrAllBannersBasic = $arrAllBannersBasic;
+        
+        // Static URLs Controller::setStaticUrls
+        $this->setStaticUrls();
     }
     
     /**
