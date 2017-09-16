00d068d74362e59f9547b2ae9206cc8557277575
Glen Langer <contao.modul@glen-langer.de>
2017-09-16 20:57:45 +0200

    Fixed #15, [Insight] Code should not be duplicated

--- src/Resources/contao/classes/BannerExternal.php
+++ src/Resources/contao/classes/BannerExternal.php
@@ -15,7 +15,7 @@
 namespace BugBuster\Banner;
 
 use BugBuster\Banner\BannerImage;
-use BugBuster\Banner\BannerHelper;
+use BugBuster\Banner\BannerTemplate;
 
 /**
  * Class BannerExternal
@@ -99,89 +99,17 @@ class BannerExternal
         return $objReturn;
     }
     
+    /**
+     * Generate Template Data
+     *
+     * @param array     $arrImageSize
+     * @param string    $FileSrc
+     * @param array     $picture
+     * @return array    $arrBanners
+     */
     public function generateTemplateData($arrImageSize, $FileSrc, $picture)
     {
-        $banner_target = ($this->objBanners->banner_target == '1') ? '' : ' target="_blank"';
-        
-        if ( strlen($this->objBanners->banner_comment) > 1 )
-        {
-            $banner_comment_pos = strpos($this->objBanners->banner_comment,"\n",1);
-            if ($banner_comment_pos !== false)
-            {
-                $this->objBanners->banner_comment = substr($this->objBanners->banner_comment,0,$banner_comment_pos);
-            }
-        }
-        
-        // Banner Seite als Ziel?
-        if ($this->objBanners->banner_jumpTo > 0)
-        {
-            $domain = \Environment::get('base');
-            $objParent = \PageModel::findWithDetails($this->objBanners->banner_jumpTo);
-            if ($objParent !== null) // is null when page not exist anymore
-            {
-                if ($objParent->domain != '')
-                {
-                    $domain = (\Environment::get('ssl') ? 'https://' : 'http://') . $objParent->domain . TL_PATH . '/';
-                }
-                // old $this->objBanners->banner_url = $domain . \Controller::generateFrontendUrl($objParent->row(), '', $objParent->language);
-                $this->objBanners->banner_url = $domain . BannerHelper::frontendUrlGenerator($objParent->row(), null, $objParent->language);
-            }
-        }
-        
-        //$arrImageSize[0]  eigene Breite
-        //$arrImageSize[1]  eigene Höhe
-        //$arrImageSize[3]  Breite und Höhe in der Form height="yyy" width="xxx"
-        //$arrImageSize[2]
-        // 1 = GIF, 2 = JPG, 3 = PNG
-        // 4 = SWF, 13 = SWC (zip-like swf file)
-        // 5 = PSD, 6 = BMP, 7 = TIFF(intel byte order), 8 = TIFF(motorola byte order)
-        // 9 = JPC, 10 = JP2, 11 = JPX, 12 = JB2, 13 = SWC, 14 = IFF
-        switch ($arrImageSize[2])
-        {
-            case 1:// GIF
-            case 2:// JPG
-            case 3:// PNG
-                $arrBanners[] = array
-                (
-                'banner_key'     => 'bid=',
-                'banner_wrap_id'    => $this->banner_cssID,
-                'banner_wrap_class' => $this->banner_class,
-                'banner_id'      => $this->objBanners->id,
-                'banner_name'    => \StringUtil::specialchars(ampersand($this->objBanners->banner_name)),
-                'banner_url'     => $this->objBanners->banner_url,
-                'banner_target'  => $banner_target,
-                'banner_comment' => \StringUtil::specialchars(ampersand($this->objBanners->banner_comment)),
-                'src'            => \StringUtil::specialchars(ampersand($FileSrc)),//specialchars(ampersand($this->urlEncode($FileSrc))),
-                'alt'            => \StringUtil::specialchars(ampersand($this->objBanners->banner_name)),
-                'size'           => $arrImageSize[3],
-                'banner_pic'     => true,
-                'banner_flash'   => false,
-                'banner_text'    => false,
-                'banner_empty'   => false,
-                'picture'        => $picture
-                );
-                break;
-            default:
-                $arrBanners[] = array
-                (
-                'banner_key'     => 'bid=',
-                'banner_wrap_id'    => $this->banner_cssID,
-                'banner_wrap_class' => $this->banner_class,
-                'banner_id'      => 0,
-                'banner_name'    => '',
-                'banner_url'     => '',
-                'banner_target'  => '',
-                'banner_comment' => '',
-                'src'            => '',
-                'alt'            => '',
-                'size'           => '',
-                'banner_pic'     => true,
-                );
-                break;
-        }//switch
-        
-        return $arrBanners;
+        return BannerTemplate::generateTemplateData($arrImageSize, $FileSrc, $picture, $this->objBanners, $this->banner_cssID, $this->banner_class);
     }
     
 }
-
