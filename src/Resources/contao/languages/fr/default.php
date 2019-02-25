5509be99616294b68ee2cedaf470af747affe840
Glen Langer <contao.modul@glen-langer.de>
2019-02-25 23:21:46 +0100

    Fixed #32, Transifex Update (fr)

---
+++ src/Resources/contao/languages/fr/default.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Translations are managed using Transifex. To create a new translation
+ * or to help to maintain an existing one, please register at transifex.com.
+ *
+ * @license http://www.gnu.org/licenses/lgpl-3.0.html LGPL
+ *
+ * @link https://www.transifex.com/signup/
+ * @link https://www.transifex.com/projects/p/contao-banner-bundle/language/fr/
+ *
+ * last-updated: 2017-09-16T01:04:02+02:00
+ */
+
+
+$GLOBALS['TL_LANG']['MSC']['tl_banner']['noBanner'] = 'Aucune bannière trouvée et aucune image par défaut de la bannière';
+
