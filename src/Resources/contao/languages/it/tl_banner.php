04922f464a7eaf19ffb548f659ed4a1d8e064ebd
Glen Langer <contao.modul@glen-langer.de>
2020-08-09 23:41:02 +0200

    Update language files (de)

--- src/Resources/contao/languages/it/tl_banner.php
+++ src/Resources/contao/languages/it/tl_banner.php
@@ -8,7 +8,7 @@
  *
  * @license http://www.gnu.org/licenses/lgpl-3.0.html LGPL
  *
- * last-updated: 2020-02-16T17:16:24+01:00
+ * last-updated: 2020-06-24T22:44:56+02:00
  */
 
 $GLOBALS['TL_LANG']['tl_banner']['1']                        = 'Priorità alta';
