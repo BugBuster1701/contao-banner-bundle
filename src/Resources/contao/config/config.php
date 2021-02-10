987cec33f77cd781a3ccb932e6a2a4b68648330a
Glen Langer <contao.modul@glen-langer.de>
2021-02-11 00:58:41 +0100

    CSS für Filter Button, #56

--- src/Resources/contao/config/config.php
+++ src/Resources/contao/config/config.php
@@ -13,8 +13,8 @@
  * @filesource
  */
 
-\define('BANNER_VERSION', '1.2');
-\define('BANNER_BUILD', '1');
+\define('BANNER_VERSION', '1.3');
+\define('BANNER_BUILD', '0');
 
 /**
  * -------------------------------------------------------------------------
