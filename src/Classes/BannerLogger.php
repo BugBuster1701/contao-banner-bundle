3522447cce9ba894350bcf6733bf70f5a5c2784c
Glen Langer <contao.modul@glen-langer.de>
2023-12-31 12:45:10 +0100

    #84, Aufruf composer ecs

--- src/Classes/BannerLogger.php
+++ src/Classes/BannerLogger.php
@@ -18,50 +18,52 @@ declare(strict_types=1);
 namespace BugBuster\BannerBundle\Classes;
 
 use Contao\CoreBundle\Monolog\ContaoContext;
-use Symfony\Component\DependencyInjection\ContainerInterface;
-use Psr\Log\LogLevel;
 use Psr\Log\LoggerInterface;
+use Psr\Log\LogLevel;
+use Symfony\Component\DependencyInjection\ContainerInterface;
 
 class BannerLogger
 {
     public function __construct(
         private readonly ContainerInterface $container,
         private LoggerInterface $bannerLogger,
-        private $contaologger)
-    {
-
+        private $contaologger,
+    ) {
     }
 
     /**
      * Add a log entry to contao system log.
      *
-     * @param string  $message  The log message
-     * @param string  $method   The function name. Typically __METHOD__
-     * @param string  $category The category name. Use constants in ContaoContext
+     * @param string $message  The log message
+     * @param string $method   The function name. Typically __METHOD__
+     * @param string $category The category name. Use constants in ContaoContext
      */
     public function logSystemLog(string $message, string $method, string $category): void
     {
-        $level = (ContaoContext::ERROR === $category ? LogLevel::ERROR : LogLevel::INFO);
+        $level = ContaoContext::ERROR === $category ? LogLevel::ERROR : LogLevel::INFO;
 
-        $this->contaologger->log($level, $message, [
-            'contao' => new ContaoContext($method, $category),
-        ]);
+        $this->contaologger->log(
+            $level,
+            $message, [
+                'contao' => new ContaoContext($method, $category),
+        ],
+        );
     }
 
     /**
      * Add a log entry to var/log/prod|test log.
      *
-     * @param string       $message  The log message
-     * @param string|bool  $class    The function name. Typically __METHOD__
-     * @param integer      $line     The line number. Typically __LINE__
-     * @param string       $level    The log level
+     * @param string      $message The log message
+     * @param string|bool $class   The function name. Typically __METHOD__
+     * @param int         $line    The line number. Typically __LINE__
+     * @param string      $level   The log level
      */
     public function logMonologLog(string $message, string $class, int $line, string $level = 'debug'): void
     {
-		if (false != $class) {
-			$this->bannerLogger->$level($message,["class" => $class.'::'.$line]);
-		} else {
-			$this->bannerLogger->$level($message,[]);
-		}
+        if (false !== $class) {
+            $this->bannerLogger->$level($message, ['class' => $class.'::'.$line]);
+        } else {
+            $this->bannerLogger->$level($message, []);
+        }
     }
 }
--- src/ContaoManager/Plugin.php
+++ src/ContaoManager/Plugin.php
@@ -19,9 +19,9 @@ namespace BugBuster\BannerBundle\ContaoManager;
 
 use Contao\CoreBundle\ContaoCoreBundle;
 use Contao\ManagerPlugin\Bundle\BundlePluginInterface;
-use Contao\ManagerPlugin\Config\ConfigPluginInterface;
 use Contao\ManagerPlugin\Bundle\Config\BundleConfig;
 use Contao\ManagerPlugin\Bundle\Parser\ParserInterface;
+use Contao\ManagerPlugin\Config\ConfigPluginInterface;
 use Contao\ManagerPlugin\Routing\RoutingPluginInterface;
 use Symfony\Component\Config\Loader\LoaderInterface;
 use Symfony\Component\Config\Loader\LoaderResolverInterface;
@@ -52,7 +52,6 @@ class Plugin implements BundlePluginInterface, RoutingPluginInterface, ConfigPlu
 
     /**
      * @throws \Exception
-     * 
      */
     public function registerContainerConfiguration(LoaderInterface $loader, array $managerConfig): void
     {
